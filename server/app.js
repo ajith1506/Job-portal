532da39ef2085f78be970f31d025197d1f5ee3b4
ajith1506 <ajithkumararun1111@gmail.com>
2024-11-29 20:21:31 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -13,9 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
 const corsOption = {
-  origin: ["http://localhost:5173", "https://ak-job-portal.netlify.app/"],
+  origin: ["http://localhost:5173", "https://ak-job-portal.netlify.app"],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 app.use(cors(corsOption));
